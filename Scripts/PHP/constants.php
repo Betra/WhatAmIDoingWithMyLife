da902f470c9a7857d18b9973ca7f6001a09caf68
Ivan <IDDorofeev@yahoo.com>
2016-10-16 22:43:12 +0300

    Init

---
+++ Scripts/PHP/constants.php
@@ -0,0 +1,3 @@
+<?php
+// Actually, mortal one can't see it, congrats.//
+constant("APIKEY","0E0BC234423D14F04ACEE607F2FCEA23");
---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>What Am I Doing With My Life?</title>
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+</head>
+<body>
+    <div id='greet'>    
+        <h1>What Am I Doing With My Life?</h1>
+        <hr>
+        <p>How much time do you play videogames and what is the better option<p>
+    </div>
+    <div id="bigbutton">
+        <form>
+        <input type="text" name="steamname" id="input" placeholder="Enter your Steam ID/login and push Enter"> <input type="image" id="enter" src="Images/enter.png" width="32">
+        </form>
+    </div>
+
+    
+</body>
+
+</html>
